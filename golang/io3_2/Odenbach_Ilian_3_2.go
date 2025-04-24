1165c37a8f53f4d92ca4ded6951ff837eea98f5b
IliHanSoLow <ilian-ode@gmx.de>
2025-04-24 13:12:51 +0200

    cryptography tasks3

---
+++ golang/io3_2/Odenbach_Ilian_3_2.go
@@ -0,0 +1,84 @@
+// Zum ausführen: go test -v *.go
+// Ich habe die print statements im code gelassen, da diese bei einem erfolgreichen Test nicht printen (ohne -v flag)
+// und den Entwicklungsprozess etwas beschreiben.
+
+package io2_2
+
+import (
+	"errors"
+	"fmt"
+	"math/bits"
+	"reflect"
+)
+
+func Q(b uint32) uint32 {
+	return b ^ bits.RotateLeft32(b, 17)
+}
+
+func new_s(s uint32, buf uint32) uint32 {
+	fmt.Printf("s: 0x%x\n", s)
+	return Q(s ^ buf)
+}
+
+func hashing(m string) uint32 {
+	var s uint32 = 0x524f464c
+	return hashing_function(m, s)
+}
+
+func hashing_function(m string, s uint32) uint32 {
+	println("Called with: ", m)
+	var buf uint32 = 0
+	//s_0
+	for x, i := range m {
+		buf |= uint32(i) << (8 * (3 - (x % 4)))
+		fmt.Printf("Char: %x\n", uint32(i))
+		fmt.Printf("Buf: 0x%x, x: %d, x%%8: %d\n", buf, x, (x % 4))
+		if (x+1)%4 == 0 {
+			s = new_s(s, buf)
+			buf = 0
+		}
+	}
+
+	if len(m)%4 != 0 {
+		buf |= 0xffffffff >> (8 * (len(m) % 4))
+		s = new_s(s, buf)
+	}
+	fmt.Printf("Final Buf: 0x%x is type %s\n", buf, reflect.TypeOf(buf))
+	fmt.Printf("Final s: 0x%x\n", s)
+	println("----------------------------------")
+	return Q(s)
+}
+
+func findSForQ(hash uint32) (uint32, error) {
+	var i uint32 = 0
+	// for ; i < 4294967295; i++ {
+	for ; i < 0xFFFFFFFF; i++ {
+		if i%1000000000 == 0 {
+			println("Calculating")
+		}
+		if Q(i) == hash {
+			fmt.Printf("Found i for Q(i) = hash with hash: 0x%x and i: 0x%x\n", hash, i)
+			return i, nil
+		}
+	}
+	return 0xDEADBEEF, errors.New("Could not found s to hash")
+}
+
+// input: mic, original_message, append_message ; output: manupulated message, mic, error
+func lea(mic uint32, original_message string, append_message string) (string, uint32, error) {
+	s, err := findSForQ(mic)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return "", 0, err
+	}
+	out_mic := hashing_function(append_message, s)
+	// Fill message up with FF until buffers close
+	if len(original_message)%4 != 0 {
+		m := []byte(original_message)
+		for len(m)%4 != 0 {
+			m = append(m, 0xFF)
+		}
+		original_message = string(m)
+	}
+	return original_message + append_message, out_mic, nil
+}
---
+++ golang/io3_2/Odenbach_Ilian_3_2_test.go
@@ -0,0 +1,100 @@
+package io2_2
+
+import (
+	"fmt"
+	"testing"
+)
+
+type TestsHashing_t struct {
+	in     string
+	should uint32
+}
+
+var hashingTests = []TestsHashing_t{
+	{in: "", should: 0xded7e2d2},
+	{in: "A", should: 0x5d725f7f},
+	{in: "AB", should: 0x5f3b5f7f},
+	{in: "ABC", should: 0x5f39137f},
+	{in: "ABCD", should: 0x5f391128},
+	{in: "ABCDE", should: 0x2f69af58},
+}
+
+type TestsLea_t struct {
+	mic uint32
+	// original_message
+	om string
+	// append_message
+	am string
+	// should mic
+	smic uint32
+}
+
+var leaTests = []TestsLea_t{
+	{mic: 0x632e4e5c, om: "abcd", am: "ef", smic: 0x0f6b8802},
+	{mic: 0x632e4e5c, om: "abcd", am: "efghijk", smic: 0x2638a819},
+	{mic: 0x0f6b8802, om: "abcdef", am: "ghijk", smic: 0},
+	{mic: 0x2638a819, om: "abcdefghijk", am: "foobar", smic: 0},
+	{mic: 0x782a826e, om: "foobar", am: "barfoo", smic: 0},
+	{mic: 0x885dc316, om: "barfoo", am: "foobar", smic: 0},
+}
+
+func TestQ1(t *testing.T) {
+	var in uint32 = 0x524f464c
+	var should uint32 = 0xded7e2d2
+	if output := Q(in); output != should {
+		t.Errorf("With INPUT: %x is OUTPUT %x , not the EXPECTED: %x", in, output, should)
+	}
+}
+
+func TestQ2(t *testing.T) {
+	var in uint32 = 0x524f464c
+	var should uint32 = 0x1b725f7d
+	if output := Q(Q(in)); output != should {
+		t.Errorf("With INPUT: %x is OUTPUT %x , not the EXPECTED: %x", in, output, should)
+	}
+}
+
+func TestQ3(t *testing.T) {
+	var in uint32 = 0x524f464c
+	var should uint32 = 0xa5886999
+	if output := Q(Q(Q(in))); output != should {
+		t.Errorf("With INPUT: %x is OUTPUT %x , not the EXPECTED: %x", in, output, should)
+	}
+}
+
+func TestHashing(t *testing.T) {
+	for _, test := range hashingTests {
+		if output := hashing(test.in); output != test.should {
+			t.Errorf("With INPUT: %q is OUTPUT %x , not the EXPECTED: %x", test.in, output, test.should)
+		}
+	}
+}
+
+func TestFindQ(t *testing.T) {
+	var hash uint32 = 0x632e4e5c
+	s, err := findSForQ(hash)
+	if err != nil {
+		t.Errorf("With Err:%s for hash: 0x%x", err, hash)
+	}
+	if Q(s) != hash {
+		t.Errorf("With hash: 0x%x, is not Q(%x)", hash, s)
+	}
+}
+
+func TestLea(t *testing.T) {
+	for _, test := range leaTests {
+		outm, outmic, err := lea(test.mic, test.om, test.am)
+		if err != nil {
+			t.Errorf("With Err:%s for input: (0x%x, %q, %q)", err, test.mic, test.om, test.am)
+		}
+		fmt.Printf("outmsg: %q, outmic: 0x%x\n", outm, outmic)
+		if test.smic != 0 {
+			if outmic != test.smic {
+				t.Errorf("With INPUT: (0x%x, %q, %q) is OUTPUT mic: %x , not the EXPECTED: mic: %x",
+					test.mic, test.om, test.am, outmic, test.smic)
+			}
+		}
+
+		fmt.Printf("----------------------------------")
+	}
+}
