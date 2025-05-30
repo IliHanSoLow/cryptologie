e15639cac5bba776ddc4d91858d180a5d918d97c
IliHanSoLow <ilian-ode@gmx.de>
2025-05-30 20:48:23 +0200

    start of week 8

--- c/io_8_2/src/crypt.c
+++ c/io_8_2/src/crypt.c
@@ -1,3 +1,19 @@
 // TODO: hash function
 //       mask generating function
-#include <crypt.h>
+#include "crypt.h"
+
+uint8_t *
+mgf(uint8_t seed, uint32_t length)
+{
+    uint64_t hLen = 160;
+    if (length > (hLen << 32))
+    {
+        return NULL;
+    }
+    char *T;
+
+    uint32_t counter;
+    while (strlen(T) < length) {
+
+    }
+}
--- c/io_8_2/src/crypt.h
+++ c/io_8_2/src/crypt.h
@@ -2,5 +2,10 @@
 #define CRYPT_H_
 
 #include <openssl/sha.h>
+#include <stdio.h>
+#include <string.h>
+#include "util.h"
+
+uint8_t *mgf(uint8_t seed, uint32_t length);
 
 #endif // CRYPT_H_
